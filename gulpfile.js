5871425658b327632d364056bbcf0cd3228a259b
Jessica Dilworth <jessicadilworth@JessicasMacBook.home>
2016-06-30 21:11:31 +0100

    set up browser sync and configured build tools

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var browserSync = require('browser-sync').create();
 
 // Lint Task
 gulp.task('lint', function() {
@@ -19,13 +20,16 @@ gulp.task('lint', function() {
 gulp.task('sass', function() {
     return gulp.src('src/css/*.scss')
         .pipe(sass())
-        .pipe(gulp.dest('dist/css'));
+        .pipe(gulp.dest('dist/css'))
+        .pipe(browserSync.reload({
+      stream: true
+    }))
 });
 
 // Concatenate & Minify JS
 gulp.task('scripts', function() {
     return gulp.src('src/js/*.js')
-        .pipe(concat('dist/js/all.js'))
+        .pipe(concat('all.js'))
         .pipe(gulp.dest('dist/js'))
         .pipe(rename('all.min.js'))
         .pipe(uglify())
@@ -33,10 +37,20 @@ gulp.task('scripts', function() {
 });
 
 // Watch Files For Changes
-gulp.task('watch', function() {
+gulp.task('watch', ['browserSync', 'sass'], function() {
     gulp.watch('src/js/*.js', ['lint', 'scripts']);
-    gulp.watch('src/scss/*.scss', ['sass']);
+    gulp.watch('src/css/*.scss', ['sass']);
+    gulp.watch('*.html', browserSync.reload);
+    gulp.watch('src/js/*.js', browserSync.reload);
 });
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: ''
+    },
+  })
+})
 
 // Default Task
 gulp.task('default', ['lint', 'sass', 'scripts', 'watch']);
